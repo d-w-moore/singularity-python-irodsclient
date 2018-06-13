46adf6a4fc0cdfd94f70d09a3dacdbfddd7da185
d-w-moore <dmoore@renci.org>
2018-06-13 07:01:04 -0400

    simplified admin_as_rodsuser to use irods env file

--- admin_as_rodsuser.py
+++ admin_as_rodsuser.py
@@ -8,27 +8,19 @@ from sys import argv as orig_argv, stderr, stdout, exit
 from getopt import GetoptError, getopt
 import json
 
-# admin_as_rodsuser.py [-z tempZone] -U rods -P rods -u alice -p /phys/path/to/file -m mdmanifest_file.json
+# admin_as_rodsuser.py -p /phys/path/to/file -m mdmanifest_file.json -u alice
 
 try:
-  opt, argv = getopt( 
-         orig_argv[1:], 'j:J:p:r:c:r:U:P:u:z:m:H:I:v:' # 'E:' <-- (disabling for now)
-  )
+  opt, argv = getopt( orig_argv[1:], 'p:r:r:u:m:v:e:' )
 except GetoptError:
   print ( '''Usage: %s [options]
   where options are:
-  \t -j json-config ( read from admin credentials file in .JSON )
-  \t -J password ( write to admin credentials file in .JSON )
   \t -h help  (print this usage statement)
-  \t -H hostname  (machine with target iRODS server for our connect)
-  \t -I portN (integer - iRODS server port # to connect to )
   \t -p physical path (file to register into iRODS)
   \t -r <resc_name>   (resource from which to register)
-  \t -U <admin_user_name>  (usually 'rods')
-  \t -P <admin_user_pw>
-  \t -E <admin_user_environment_file.json>
+  \t -e <admin user irods_environment.json> look in ~/.irods by default
+  \t -a <admin user auth file> look in ~/.irods/.irodsA by default
   \t -u <client_user> (client user on behalf of which we are working)
-  \t -z <iRODS_zone>
   \t -m <md_manifest> (.JSON file - destination collection & metadata ops.)
   \t -v N (set verbosity level)
   ''' % (orig_argv[0],) , file = stderr )
@@ -38,51 +30,11 @@ argv.insert(0,orig_argv[0])
 options = {}
 options.update( opt )
 
-#-=-=-=-=-=- Change admin password file
-
-if options.get('-J','') and (options.get('-u') is None): 
-  new_pw = options.get('-P','')
-  new_user =  options.get('-U',None)
-  if type(new_user) is str and len(new_user) == 0:
-    print ("ERROR: need admin username of >= 1 character", file = sys.stderr)
-    raise SystemExit(1)
-  try:
-    with open (options['-J']) as f:
-      admin_credentials =  json.load(f)
-      admin_credentials ['admin_password'] = new_pw if len(new_pw) else None
-      if new_user is not None:
-        admin_credentials ['admin_username'] = new_user
-    with open (options['-J'],'w') as f:
-      json.dump(admin_credentials,f,indent=2)
-  except: 
-    print ("Failed to change admin credentials.(For reasons.)",file=sys.stderr)
-    exit(3)
-  finally:
-    exit(0)
-
-admin_username = options.get('-U')
-admin_password = options.get('-P')   
-
-if not(admin_username) or not(admin_username) :
-  default_admin_creds_file = expanduser('~/compute/admin_as_user.json')
-  jsonConfigFile = options.get('-j',default_admin_creds_file)
-  with open(jsonConfigFile) as f:
-    jsonConfig = json.load(f)
-    admin_username = str(jsonConfig.get('admin_username'))
-    admin_password = str(jsonConfig.get('admin_password'))
-
-assert ( admin_username and admin_password )
-
-irods_server_host = options.get('-H','localhost')
-irods_server_port = int(options.get('-I','1247'))
-
 user = options.get('-u','')            # - client user
-zone = options.get('-z', 'tempZone' )  # - iRODS zone for compute-to-data ex.
-
-env_file = options.get( '-E', '' )
-if env_file:
-  print('"-E" is presently a disabled feature', file=sys.stderr)
-  raise SystemExit(2)
+env_file = options.get( '-e', '')
+if env_file == '-': env_file = expanduser('~/.irods/irods_environment.json' )
+auth_file = options.get( '-a', '' )
+if auth_file == '-': auth_file = expanduser('~/.irods/.irodsA')
 
 phyP = options.get('-p' ,'')
 rescN = options.get('-r','')
@@ -97,27 +49,22 @@ if verbosity >= 2:
 
 options_for_session = { 'client_user': user,
                         'irods_env_file' :  env_file,
-                        'password': admin_password
+                        'irods_authentication_file' :  auth_file,
                       }
 session = None
 
-for k in ('client_user', 
-          'password',
-          'irods_env_file' ):
-  if not options_for_session[k]: del options_for_session[k] 
+k = options_for_session.keys()
+for k_ in k: 
+  if not options_for_session[k_]:
+    del options_for_session[k_]
 
 if not session:
-  if not user:
-    session = iRODSSession ( host= irods_server_host , port = irods_server_port,
-                             user = admin_username , password = admin_password,
-                             zone = zone, *options_for_session )
-  else:
-    session = iRODSSession( # host='localhost', port=1247,
-		host = irods_server_host , port = irods_server_port , 
-                user = admin_username , password = admin_password ,
-                zone = zone , client_user = user )
-                          # user = admin_user , password = admin_pass,
-                          # zone = zone, client_user = user
+    session = iRODSSession( **options_for_session )
+
+if not user : user = 'rods'
+else:
+  if verbosity >= 1:
+    print( "\tActing for user '{}'".format(session.username) , file = stderr )
 
 if session:
   test_user = (user or admin_username)
@@ -125,9 +72,6 @@ if session:
   collObj = session.collections.get( collName  )
   dataObjs = collObj.data_objects
   print( "{!r}".format(dataObjs) , file = stderr )
-
-if verbosity >= 1:
-  print( "\tActing for user '{}'".format(session.username) , file = stderr )
 
 #==============================================
 

9784537074c2f8be5b6de73cb88221463e5a5d20
d-w-moore <dmoore@renci.org>
2018-06-15 15:15:36 -0400

    fixed some problems with admin_as_rodsuser.py

--- admin_as_rodsuser.py
+++ admin_as_rodsuser.py
@@ -1,28 +1,73 @@
 
 from __future__ import print_function
+
 import os, sys
 import irods
+from irods.meta import iRODSMeta
 from irods.session import iRODSSession
+from irods.data_object import iRODSReplica
 from os.path import expanduser, curdir, sep ,abspath, join
 from sys import argv as orig_argv, stderr, stdout, exit
 from getopt import GetoptError, getopt
 import json
 
-# admin_as_rodsuser.py -p /phys/path/to/file -m mdmanifest_file.json -u alice
+
+# ////////////////
+#
+# Sample usage:
+#
+# admin_as_rodsuser.py [ opts ... ] [ -u clientUserName ]
+#   miscellaneous opts:
+#             -p /file/to/be/registered 
+#             -m mdmanifest_file.json 
+#             -u clientUserName
+#
+# ////////////////
+
+def replicate_and_list_good_replicas (session , logical_path ,  resc_name='',  # -> inputs
+                                      list_for_tally = () ):                   # -> output
+
+  data_obj = session.data_objects.get( logical_path )
+  data_obj .replicate (resc_name )
+
+  data_obj = session.data_objects.get( logical_path )
+
+  replicas = [ repl for repl in data_obj.replicas 
+               if repl.status == '1' ]
+  
+  if type( list_for_tally ) in (list, tuple):
+
+    existing_repl_ids = [ x.number for x in list_for_tally if type(x) is iRODSReplica ]
+
+    new_list = list (list_for_tally)
+
+    for x in replicas:
+      if x.number not in existing_repl_ids:
+        new_list.append (x)
+
+    if type(list_for_tally) is list: list_for_tally[:] = new_list
+
+  return new_list
+
+#--------------------------------------------------------
 
 try:
-  opt, argv = getopt( orig_argv[1:], 'p:r:r:u:m:v:e:' )
+  opt, argv = getopt( orig_argv[1:], 'p:r:f:u:m:v:e:t:' )
 except GetoptError:
   print ( '''Usage: %s [options]
   where options are:
-  \t -h help  (print this usage statement)
-  \t -p physical path (file to register into iRODS)
-  \t -r <resc_name>   (resource from which to register)
-  \t -e <admin user irods_environment.json> look in ~/.irods by default
-  \t -a <admin user auth file> look in ~/.irods/.irodsA by default
-  \t -u <client_user> (client user on behalf of which we are working)
-  \t -m <md_manifest> (.JSON file - destination collection & metadata ops.)
-  \t -v N (set verbosity level)
+   -h help  (print this usage statement)
+   -p physical path (file to register into iRODS)
+   -r immediate_resc_name
+        (name of storage resource for immediate registration of product)
+   -f final_resc_name
+        (name of storage resource for long-term storage of product)
+   -e <admin user irods_environment.json> look in ~/.irods by default
+   -a <admin user auth file> look in ~/.irods/.irodsA by default
+   -u <client_user> (client user on behalf of which we are working)
+   -m <md_manifest> (.JSON file - destination collection & metadata ops.)
+   -v N (set verbosity level)
+   -t <g|m|l|t> m - add metadata ; g - register products ; l - replicate ; t - trim
   ''' % (orig_argv[0],) , file = stderr )
   exit(1)
 
@@ -31,13 +76,23 @@ options = {}
 options.update( opt )
 
 user = options.get('-u','')            # - client user
+
 env_file = options.get( '-e', '')
-if env_file == '-': env_file = expanduser('~/.irods/irods_environment.json' )
-auth_file = options.get( '-a', '' )
-if auth_file == '-': auth_file = expanduser('~/.irods/.irodsA')
+
+if not(env_file) or env_file == '-':
+  env_file = expanduser('~/.irods/irods_environment.json' )
+
+auth_file = options.get( '-a' )
+
+if auth_file is not None:
+  if auth_file in ('', '-'):
+    auth_file = expanduser('~/.irods/.irodsA')
+else:
+  auth_file = ''
 
 phyP = options.get('-p' ,'')
 rescN = options.get('-r','')
+long_term_rescN = options.get('-f','')
 
 verbosity = int( options.get('-v','0') )
 if verbosity >= 2:
@@ -66,18 +121,12 @@ else:
   if verbosity >= 1:
     print( "\tActing for user '{}'".format(session.username) , file = stderr )
 
-if session:
-  test_user = (user or admin_username)
-  collName = '/tempZone/home/{}'.format(test_user)
-  collObj = session.collections.get( collName  )
-  dataObjs = collObj.data_objects
-  print( "{!r}".format(dataObjs) , file = stderr )
-
-#==============================================
-
 md_manifest_file = options['-m']
 
-if not phyP: exit(1)
+if not(phyP) or not(os.path.isfile(phyP)):
+  print ("invalid physical file path '{}'\n\t" \
+         "as source for iRODS register()".format(phyP), file=sys.stderr)
+  raise SystemExit(1)
 
 mdManifest = {}
 with open(md_manifest_file,'r') as f:
@@ -93,15 +142,16 @@ except Exception as e:
   print(  repr(e) + "\n -- trying create" ,file=stderr)
   parentColl = session.collections.create(parentCollName)
 
+assert parentColl, "Could not find collection in which to register " +\
+                   "{}".format(phyP)
+
 if not(rescN):
   del register_opts['rescName'] 
 else:
   register_opts['rescName'] = rescN
 
-assert os.path.isfile( phyP )
-
-data_object_name = os.path.basename (phyP)
-logP = parentColl.path + "/" + data_object_name
+data_object_basename = os.path.basename (phyP)
+logP = parentColl.path + "/" + data_object_basename
   
 answer = 'y'
 
@@ -111,11 +161,50 @@ if verbosity >= 1:
   print ('logP     = "{}" ' .format( logP), file = stderr )
   print ('rescN    = "{}" ' .format( rescN), file = stderr )
   print ('opts     = "{!r}" ' .format( register_opts), file = stderr )
+
   if verbosity >= 2:
     assert _input is not None
     answer = _input('proceed (y/n) -> ')
 
 if answer.upper().strip() == 'Y':
-  print ( '** Registering **' , file = stderr )
-  session.data_objects.register ( phyP , logP , **register_opts )
 
+  task_options = options.get('-t','gmtl')
+
+  # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+  if 'g' in task_options:
+    print ( '** Registering **' , file = stderr )
+    session.data_objects.register ( phyP , logP , **register_opts )
+
+  # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+  if 'm' in task_options:
+    print ( '** Setting metadata **', file = stderr)
+    data_obj = session.data_objects.get( logP )
+    physical_path_for_data_obj = data_obj.path
+    for md_op in mdManifest.get("operation", [] ):
+      action = md_op["action"] 
+      if action == 'ADD':
+        if md_op["irodsPath"] == os.path.basename ( data_obj.path ):
+          md_record = iRODSMeta ( md_op["attribute"],md_op["value"],md_op["unit"] )
+          data_obj.metadata.add ( md_record )
+      else:
+        print (" unimplemented feature '{}' requested in mdmanifest file '{}'" )
+      
+# -- replicate product to long term storage
+
+  if 'l' in task_options:
+
+    repls_list = replicate_and_list_good_replicas (session , logP ,  long_term_rescN )
+
+# -- remove any redundant product replicas
+
+  if 't' in task_options:
+
+    redundant_repls = [ r for r in repls_list if r.resource_name != long_term_rescN ]
+
+    if redundant_repls and len(redundant_repls) < len(repls_list):
+      for r in redundant_repls:
+         data_obj.unlink ( replNum = r.number)
+
+  1
